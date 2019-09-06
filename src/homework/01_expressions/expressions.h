c22ea48d8838b926098007ee3a420cde9259a9db
Cody Kobayashi <cody.kobayashi@g.austincc.edu>
2019-09-05 23:01:14 -0500

    Homework 1 completed.

--- src/homework/01_expressions/expressions.h
+++ src/homework/01_expressions/expressions.h
@@ -2,7 +2,7 @@
 int add_numbers(int num1, int num2);
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
-
+int multiply_numbers(int num1, int num2); // Takes num1 and num 2 parameter and multiplies them together
 
 
 
