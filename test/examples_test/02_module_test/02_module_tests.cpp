e0490cb68dcaa8194f6da276ab95b87fd8414dac
8584017 <8584017@RRC2220-11>
2019-09-09 19:00:51 -0500

    Classwork, if else and case. Assignment 2

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.cpp"
+#include "if_else.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -10,4 +11,14 @@ TEST_CASE("Test is_even function")
 {
 	REQUIRE(is_even(2) == true);
 	REQUIRE(is_even(20000000000000003) == false);
+}
+
+TEST_CASE("Get generation")
+{
+	REQUIRE(get_generation(2000) == "Centenial");
+	REQUIRE(get_generation(2043653400) == "Invalid");
+	REQUIRE(get_generation(1975) == "Generation X");
+	REQUIRE(get_generation(1960) == "Baby boomer");
+	REQUIRE(get_generation(1940) == "Silent Generation");
+	REQUIRE(get_generation(1932) == "Silent Generation");
 }
