a0561a3f80511b1c8812d08646ad1619ca8557ff
8584017 <8584017@RRC2220-11>
2019-09-11 18:39:37 -0500

    Homework 2 completed/Module 2

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.cpp"
 #include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -21,4 +22,13 @@ TEST_CASE("Get generation")
 	REQUIRE(get_generation(1960) == "Baby boomer");
 	REQUIRE(get_generation(1940) == "Silent Generation");
 	REQUIRE(get_generation(1932) == "Silent Generation");
+}
+
+TEST_CASE("test menu function")
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(3321) == "Invalid");
 }
--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "../../../homework/02_decisions/decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify and test GPA") {
+	REQUIRE(calculate_gpa(12, 45) == 3.75);
+	REQUIRE(calculate_gpa(120, 390) == 3.25);
+	REQUIRE(calculate_gpa(90, 180) == 2.00);
+}
