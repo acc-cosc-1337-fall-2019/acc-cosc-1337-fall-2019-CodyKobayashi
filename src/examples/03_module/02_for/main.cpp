be6c71e35f4cd3c6a0196b50047c9950860951e4
8584017 <8584017@RRC2220-11>
2019-09-16 19:29:18 -0500

    Loops, such as do while, while, and for's Module_03

--- src/examples/03_module/02_for/main.cpp
+++ src/examples/03_module/02_for/main.cpp
@@ -2,6 +2,7 @@
 
 int main() 
 {
+	display_numbers(5000000);
 	
 	return 0;
 }
--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,14 +1,44 @@
 #include<iostream>
 #include "do_while.h"
-#include "switch.h"
+//#include "switch.h"
+
 
 using std::cout;
 using std::cin;
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+	auto user_choice = 'y';
+	do
+	{
+		cout << "Loop again? y/n";
+		cin >> user_choice;
+	} 
+	while (user_choice == 'y' || user_choice == 'Y');
+}
 
 
+string menu(int menu_option)
+{
+	switch (menu_option)
+	{
+	case 1:
+		//If
+		return "Option 1";
+	case 2:
+		return "Option 2";
+	case 3:
+		return "Option 3";
+	case 4:
+		return "Option 4";
+	default:
+		return "Invalid";
+	}
+
+	return string();
+}
 //Write code for function run_menu that prompts  user for a 
 //number from 1 to 4 and displays the option user selected.
 /*
@@ -17,3 +47,25 @@ folder.
 
 */
 
+void run_menu()
+{
+	auto user_choice = 'y';
+	auto choice = 0;
+
+	do
+	{
+		cout << "Enter menu option: ";
+		cin >> choice;
+
+		while (choice < 1 || choice > 4) 
+		{
+			cout << "Enter menu option: ";
+			cin >> choice;
+		}
+
+		cout << menu(choice) << "\n";
+		cout << "Continue y or n";
+		cin >> user_choice;
+
+	} while (choice == 'y' || choice == 'Y');
+}
