262614760a58eae72b53caf46a131ec33975a225
8584017 <8584017@RRC2220-11>
2019-09-23 19:06:22 -0500

    Examples_3, functions, reference and vector iteration

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -9,27 +9,63 @@ Write code for function loop_vector_w_index with no parameters.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
+void loop_vector_w_index()
+{
+	vector<int> nums = {3,5,99,8};
 
-
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by value parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
+void loop_vector_w_index(std::vector<int> nums)
+{
 
-
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by reference parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
-
+void loop_vector_w_index_ref(std::vector<int>& nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int const pass by reference parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
+void loop_vector_w_index_const(const std::vector<int>& nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
+/*
+Iterate a vector with a for ranged loop (use auto)
+*/
+void loop_vector_w_for_ranged(const std::vector<int>& nums) 
+{
+	for (auto num : nums)
+	{
+		std::cout << num << "\n";
+	}
+}
