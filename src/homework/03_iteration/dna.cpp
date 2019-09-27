4901714b0223ef5e7f3dd3260bd289188604e5f7
Cody Kobayashi <cody.kobayashi@g.austincc.edu>
2019-09-26 23:52:17 -0500

    HomeWork 4 Loop issue

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -1,4 +1,6 @@
 #include "dna.h"
+#include <cstring>
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,8 +8,21 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string & dna)
+{
+	double dna_length = dna.size();
+	double gc_count = 0;
 
+	for (int i = 0; i < dna_length; i++)
+	{
+		if (dna[i] == 'G' || dna[i] == 'C')
+		{
+			gc_count += 1;
+		}
+	}
 
+	return gc_count / dna_length;
+}
 
 
 /*
@@ -15,6 +30,15 @@ Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+string get_reverse_string(string dna)
+{
+	string dna_reversed = "";
+	for (int i = dna.size() - 1; i >= 0; i--)
+	{
+		dna_reversed += dna[i];
+	}
+	return dna_reversed;
+}
 
 
 /*
@@ -27,4 +51,31 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna)
+{
+	string dna_complement = get_reverse_string(dna);
 
+	for (int i = 0; i < dna_complement.size(); i++)
+	{
+		switch (dna_complement[i])
+		{
+		case 'A':
+			dna_complement[i] = 'T';
+			break;
+
+		case 'T':
+			dna_complement[i] = 'A';
+			break;
+
+		case 'C':
+			dna_complement[i] = 'G';
+			break;
+
+		case 'G':
+			dna_complement[i] = 'C';
+			break;
+		}
+	}
+
+	return dna_complement;
+}
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,11 @@
 //write include statements
+#include <iostream>
+#include"dna.h"
 
 //write using statements
-
+using std::string;
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +16,47 @@ user enters a y or Y.
 */
 int main() 
 {
+	string dna = "";
+	string contin = "y";
+	double gc_content = 0.0;
+	string dna_complement = "";
+	int user_choice = 0;
+
+	do
+	{
+
+		cout << "Please enter 1 to get GC Content or enter 2 to get DNA Complement: ";
+		cin >> user_choice;
+
+		switch (user_choice)
+		{
+			
+		case 1:
+			cout << "Enter the DNA sequence you want to get the GC content of: ";
+			cin >> dna;
+
+			gc_content = get_gc_content(dna);
+			cout << "The GC content is: " << gc_content * 100 << "%\n";
+			break;
+
+		case 2:
+			cout << "Enter the DNA sequence you want to get the DNA complement: ";
+			cin >> dna;
+
+			dna_complement = get_dna_complement(dna);
+			cout << "DNA complement is: " << dna_complement << "\n";
+			break;
+
+		default:
+			cout << "Invalid choice\n";
+			break;
+		}
+
+
+
+		cout << "Do you wish to continue? Y/N:";
+		cin >> contin;
+	} while (contin == "y" || contin == "Y");
+
 	return 0;
 }
